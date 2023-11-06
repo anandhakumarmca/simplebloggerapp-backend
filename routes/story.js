73be0d846e8c597c46ba6c5da287040ca5aa55d2
Anandh Kumar <accetanandhmca@gmail.com>
2023-11-06 16:26:25 +0530

    changes reverted

--- routes/story.js
+++ routes/story.js
@@ -1,19 +1,13 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.js";
-import {
-  addStory,
-  deleteStory,
-  editStory,
-  getAllStories,
-  getStoryById,
-} from "../controllers/story.js";
+import { addStory, deleteStory, editStory, getAllStories, getStoryById } from "../controllers/story.js";
 
 const router = express.Router();
 
 router.post("/addstory", protectRoute, addStory);
 router.get("/getAllStories", getAllStories);
-router.get("/:id", protectRoute, getStoryById);
-router.put("/:id", protectRoute, editStory);
-router.delete("/:id", protectRoute, deleteStory);
+router.get("/:id", getStoryById);
+router.put("/:id", editStory);
+router.delete("/:id",protectRoute, deleteStory);
 
 export const storyRouter = router;
